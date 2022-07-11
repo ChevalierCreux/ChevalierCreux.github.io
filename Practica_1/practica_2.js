39effd543b802cef55792c5050e12da3a1e4c10d
ChevalierCreux <103346604+ChevalierCreux@users.noreply.github.com>
2022-07-11 07:18:13 -0500

    Delete Practica_1 directory

--- Practica_1/practica_2.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("boton").addEventListener("click",function(){
-    let nom = document.getElementById("nombre").value
-    let tel = document.getElementById("numero").value
-
-    document.getElementById("salida").innerHTML= "Se hara llegar en breves momentos el codigo de cangeo al siguiente correo " + tel + " y la clave de seg al ID " + nom + " para poder cangear el juego. Si no le llegar el correo de confirmacion porfavor volver a ingresar los datos."
-})
